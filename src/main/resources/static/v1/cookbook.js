13c92732a24e73ba6e9643d0326d2769e35bf2d2
Luis A Castaneda <lcastaa.aql@gmail.com>
2023-09-02 07:38:04 -0700

    added style and functionality to the recipes in each cookbook the user may have and it includes a scroll bar to see all the steps and ingredients

--- src/main/resources/static/v1/cookbook.js
+++ src/main/resources/static/v1/cookbook.js
@@ -1,10 +1,13 @@
+const recipeList = document.getElementById("recipeList");
 // Access the "message" property from the server
 var data = dataFromServer.cookBookId;
 
-async function getRecipes(cookBookId) {
+// Function to fetch recipes from an API or server
+async function fetchRecipes() {
     const token = sessionStorage.getItem("token");
+    console.log(data);
     try {
-        const response = await fetch(`/api/v1/recipe?cookBookId=${cookBookId}`, {
+        const response = await fetch(`/api/v1/recipe?cookBookId=${data}`, {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${token}`,
@@ -13,8 +16,8 @@ async function getRecipes(cookBookId) {
         });
 
         if (response.ok) {
-            const fetchedRecipes = await response.json();
-            return fetchedRecipes;
+            const recipes = await response.json();
+            return recipes;
         } else {
             throw new Error(`Failed to fetch recipes. HTTP status: ${response.status}`);
         }
@@ -23,47 +26,66 @@ async function getRecipes(cookBookId) {
     }
 }
 
-async function displayRecipes(cookBookId) {
+// Function to fetch and populate the recipe list
+async function populateRecipeList() {
+
     try {
-        const recipes = await getRecipes(cookBookId);
+        // Fetch recipes from an API or server (replace with your API endpoint)
+        const recipes = await fetchRecipes();
 
-        const recipeList = document.getElementById("recipeList");
-        recipeList.innerHTML = ""; // Clear existing recipes
+        // Clear existing recipe list
+        recipeList.innerHTML = "";
 
+        // Populate the list with fetched recipes
         recipes.forEach(recipe => {
             const li = document.createElement("li");
+            const recipeLink = document.createElement("a");
+            recipeLink.style.fontSize = "20px"; // Adjust the font size as needed
+            // Set the recipe name as the link text
+            recipeLink.textContent = recipe.name;
 
-            // Create the recipe HTML structure
-            let recipeHTML = `<div class="recipe-item">`;
-            recipeHTML += `<h3>${recipe.name}</h3>`;
+            // Add an event listener to handle click on the recipe link
+            recipeLink.addEventListener("click", () => {
+                // Toggle the visibility of the recipe details
+                const recipeDetails = document.getElementById(`recipe-${recipe.id}`);
+                if (recipeDetails.style.display === "none" || !recipeDetails.style.display) {
+                    recipeDetails.style.display = "block";
+                } else {
+                    recipeDetails.style.display = "none";
+                }
+            });
 
-            if (recipe.ingredients && Array.isArray(recipe.ingredients)) {
-                recipeHTML += `<h4>Ingredients:</h4>`;
-                recipeHTML += `<ul>`;
-                recipeHTML += recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
-                recipeHTML += `</ul>`;
-            } else {
-                recipeHTML += `<p>No ingredients listed</p>`;
-            }
+            // Append the link to the list item
+            li.appendChild(recipeLink);
 
-            if (recipe.steps && Array.isArray(recipe.steps)) {
-                recipeHTML += `<h4>Steps:</h4>`;
-                recipeHTML += `<ol>`;
-                recipeHTML += recipe.steps.map(step => `<li>${step}</li>`).join('');
-                recipeHTML += `</ol>`;
-            } else {
-                recipeHTML += `<p>No steps provided</p>`;
-            }
+            // Create a div for the recipe details
+            const recipeDetails = document.createElement("div");
+            recipeDetails.id = `recipe-${recipe.id}`;
+            recipeDetails.classList.add("scroll-container"); // Add the scroll-container class
+            recipeDetails.style.display = "none"; // Initially hide the details
 
-            recipeHTML += `</div>`;
+            // Add the recipe details content
+            recipeDetails.innerHTML = `
+                <p>Ingredients:</p>
+                <ul>
+                    ${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                </ul>
+                <p>Steps:</p>
+                <ol>
+                    ${recipe.steps.map(step => `<li>${step}</li>`).join('')}
+                </ol>
+            `;
 
-            li.innerHTML = recipeHTML;
+            // Append the recipe details to the list item
+            li.appendChild(recipeDetails);
+
+            // Append the list item to the recipe list
             recipeList.appendChild(li);
         });
     } catch (error) {
-        console.error("Error fetching and displaying recipes:", error);
+        console.error("Error fetching and populating recipes:", error);
     }
 }
 
-// Call the displayRecipes function with the cookBookId from the server
-displayRecipes(dataFromServer.cookBookId);
+// Call the populateRecipeList function to populate the list
+populateRecipeList();
