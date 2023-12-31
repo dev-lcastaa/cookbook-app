4739ba6438bdeb5e22d6fc7658d763f1cac43970
Luis Castaneda <lcastaa.aql@gmail.com>
2023-12-31 13:28:46 -0800

    Reconfiugred the API Key lookup in Enviornment

--- src/main/java/xyz/aqlabs/cookbook/CookbookApplication.java
+++ src/main/java/xyz/aqlabs/cookbook/CookbookApplication.java
@@ -1,15 +1,31 @@
 package xyz.aqlabs.cookbook;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class CookbookApplication {
-	public static void main(String[] args) {
-			SpringApplication.run(CookbookApplication.class);
-	}
 
+	@Autowired
+	static Environment env;
+
+
+	public static void main(String[] args) {
+
+		String key = env.getProperty("OPENAI_API_KEY");
+
+		try{
+			if(!key.isEmpty())
+				SpringApplication.run(CookbookApplication.class);
+			else
+				System.out.print("API KEY WAS NOT FOUND IN ENVIRONMENT");
+		}catch (Exception e){
+			System.err.println("Api Key Check resulted in error: " + e.getMessage());
+		}
+	}
 }
