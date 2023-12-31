4ff0f5a061ca3080d6cf1091112f090478884f53
Luis Castaneda <lcastaa.aql@gmail.com>
2023-12-31 10:32:46 -0800

    Testing Pipeline

--- src/main/java/xyz/aqlabs/cookbook/CookbookApplication.java
+++ src/main/java/xyz/aqlabs/cookbook/CookbookApplication.java
@@ -9,9 +9,6 @@ import java.util.Scanner;
 @SpringBootApplication
 public class CookbookApplication {
 	public static void main(String[] args) {
-
-
-
 			SpringApplication.run(CookbookApplication.class);
 	}
 
